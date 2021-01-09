d9fcb89ed5de53ca5cd84fd02d6f20309d4c7612
kazuhiro4949 <kazuhiro.hayashi.49@gmail.com>
2021-01-09 22:19:14 +0900

    Initial Commit

---
+++ MakeSwiftCodeOpen/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  MakeSwiftCodeOpen
+//
+//  Created by 林和弘 on 2021/01/09.
+//
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ MakeSwiftCodeOpen/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  MakeSwiftCodeOpen
+//
+//  Created by 林和弘 on 2021/01/09.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
